3443eb70bb69551f2e0631eba5286b240630e4ef
Hugo Marques <biohmarques@gmail.com>
2019-05-17 06:52:28 -0300

    New version created, removing the need to use the "skip" argument.

--- R/ORFID_import.R
+++ R/ORFID_import.R
@@ -3,10 +3,14 @@
 ## Creating a function to import the data
 #require(tidyverse)
 
-ORFID_import <- function(file, delim = "\t", skip = 3){
+ORFID_import <- function(file, delim = "\t", skip = NULL){
     
     if (!(delim %in% c("\t", ","))) {
         stop("The column separator must be either tab '\' or ','")
+    }
+    
+    if (is.null(skip)) {
+        stop("The number of lines to skip before reading data is mandatory. Count the number of the lines before the data header in the txt file to be imported and assigning this value for the argument 'skip'.")
     }
         
     raw_data <- read_delim(file, delim = delim, skip = skip)
@@ -21,5 +25,34 @@ ORFID_import <- function(file, delim = "\t", skip = 3){
     
 }
 
+###############################################################################
+
+# alternative
+
+ORFID_import <- function(file, delim = "\t"){
+    
+    if (!(delim %in% c("\t", ","))) {
+        stop("The column separator must be either tab '\' or ','")
+    }
+    
+    raw_data <- read_delim(file, delim = "\t", skip = grep("--------- Upload", readLines(file)))
+    
+    PIT_data <<- raw_data %>%
+        filter(DTY == "S" | DTY == "I") %>%
+        mutate(DUR=parse_time(DUR, '%H:%M:%OS')) %>%
+        mutate(TTY=as.factor(TTY)) %>%
+        mutate(SCD=as.factor(SCD))
+    
+    return(glimpse(PIT_data))
+    
+}
 
 ###############################################################################
+
+
+# validating the function
+
+setwd("C:/Users/hmarq/Desktop")
+list.files()
+
+ORFID_import("C:/Users/hmarq/Desktop/SCD_BRA.txt")
