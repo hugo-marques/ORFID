764cd4f958d1e5089b967dba7e9647f2ffff854c
Hugo Marques <biohmarques@gmail.com>
2019-05-17 21:03:09 -0300

    Script for testing functions. All versions are ok.

---
+++ R/testing_functions.R
@@ -0,0 +1,27 @@
+#Testing_functions
+
+setwd("C:/Users/hmarq/Desktop")
+list.files()
+
+#  Importing thw data:
+
+data_USA <- import_ORFID("C:/Users/hmarq/Desktop/SCD_USA_t.txt", delim = "\t")
+
+data_BRA <- import_ORFID("C:/Users/hmarq/Desktop/SCD_BRA_t.txt", delim = "\t")
+
+# Combining the data:
+
+readers <- list(data_USA, data_BRA)
+
+bla<- join_multireader_data(readers)
+
+readers2 <- list(bla, data_BRA)
+
+blabla <- join_multireader_data(readers2)
+
+
+TAG_info(bla)
+
+TAG_info(blabla)
+
+export_excel_ORFID(bla)
