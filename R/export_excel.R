40ccc7e06a64c234dd4f0fd81c5363eebc9ba90f
Hugo Marques <biohmarques@gmail.com>
2019-05-18 04:48:04 -0300

    Improvements in the function's details.

--- R/export_excel.R
+++ R/export_excel.R
@@ -2,12 +2,22 @@
 #'
 #' @description Function to export PIT_data data frames as a xlsx file to user working directory  
 #' @param x PIT_data data frame to be exported.
-#' @details File is saved in the user-specified working directory.
+#' @details The file will be saved in the user-specified working directory.
 #' @return Returns an .xlsx file.
-#' @author Hugo Marques
+#' @author Hugo Marques <biohmarques@@gmail.com>
 #' @examples
-#' #export a PIT_data data frame to the working directory.
+#' #  Exporting a PIT_data data frame to the working directory.
 #' 
+#' #  Import the ORFID reader data to the user working directory
+#' \dontrun{} data_BRA <- import_ORFID("~/Desktop/SCD_BRA.txt")
+#' 
+#' #  Create the list containing the imported files:
+#' \dontrun{} readers list(data_USA, data_BRA)
+#' 
+#' #  Combine the files:
+#' \dontrun{} data <- join_multireader_data(readers)
+#' 
+#' #  Export the multi reader data as a .xlsx file:
 #' \dontrun{} export_excel_ORFID(PIT_data)
 #' 
 #' @export
