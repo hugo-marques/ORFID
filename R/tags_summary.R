fbd7157ecd42c74d67f10d72e24a0fd358456c78
Hugo Marques <biohmarques@gmail.com>
2019-05-22 00:33:15 -0300

    Bug fixed.

--- R/tags_summary.R
+++ R/tags_summary.R
@@ -1,9 +1,9 @@
 #' @title Summarizing tag-specific info of \code{\link{import_ORFID}}
 #' @description Function for getting info about the tags recorded in a PIT_data data frame from \code{\link{import_ORFID}} function (number of unique tags, tags type and records per tag).
 #' @param x data frame to be analyzed.
-#' @param tag variable of x with the tag ID
-#' @param time variable of x with the detection time. For ORFID data it can be the either arrival (default = ARR) or departure (default = DEP).
-#' @param type variable of x with the tag type (ORFID default = TTY)
+# #' @param tag variable of x with the tag ID
+# #' @param time variable of x with the detection time. For ORFID data it can be the either arrival (default = ARR) or departure (default = DEP).
+# #' @param type variable of x with the tag type (ORFID default = TTY)
 #' @details A data frame is created in the user environment with the data grouped by TAG IDs.
 #' @return Returns a tibble object.
 #' @author Hugo Marques <biohmarques@@gmail.com>
@@ -31,52 +31,40 @@
 
 ###############################################################################
 
-tag_info <- function(x, tag, time, type){
-    
+tag_info <- function(x){
+    #, tag = "TAG", time = "ARR", type = "TTY"
     #attach(x)
     
-    if(!(tag %in% names(x))){
+    if(!("TAG" %in% names(x))){
         stop("The Tag number (TAG) is required.")
     }
     
     PIT <- x %>%
-        dplyr::group_by(tag) %>%
-        dplyr::add_count(tag) %>%
-        dplyr::distinct(tag, .keep_all = T) %>%
-        dplyr::select(tag, n) %>%
+        dplyr::group_by(TAG) %>%
+        dplyr::add_count(TAG) %>%
+        dplyr::distinct(TAG, .keep_all = T) %>%
+        #dplyr::select(TAG, n) %>%
         dplyr::rename(REC = n) %>%
-        dplyr::mutate(FIR = first(time)) %>%
-        dplyr::mutate(LAS = last(time))
+        dplyr::mutate(FIR = first(ARR)) %>%
+        dplyr::mutate(LAS = last(ARR))
     
-        if(missing(type)){
-            
-            PIT <- PIT %>%
-                dplyr::select(tag, REC, FIR, LAS)
-            
-            message("
-                    TAG: tag ID
-                    REC: number of records
-                    FIR: first record
-                    LAS: last record
-                    ")
-            
-    } else {
-        if(type %in% names(PIT)){
-            
-            PIT <- PIT %>%
-                dplyr::select(tag, type, REC, FIR, LAS)
-            
-            message("
-                    TAG: tag ID
-                    TTY: tag type (A = Animal (ICAR), R = Read-only, W = Writable, P = Phantom)
-                    REC: number of records
-                    FIR: first record
-                    LAS: last record
-                    ")
+    
+    if("TTY" %in% names(PIT)){
+        
+        PIT <- PIT %>%
+            dplyr::select(TAG, TTY, REC, FIR, LAS)
+        
+        message("
+                TAG: tag ID
+                TTY: tag type (A = Animal (ICAR), R = Read-only, W = Writable, P = Phantom)
+                REC: number of records
+                FIR: first record
+                LAS: last record
+                ")
             
         } else {
             PIT <- PIT %>%
-                dplyr::select(tag, REC, FIR, LAS)
+                dplyr::select(TAG, REC, FIR, LAS)
             
             message("
                     TAG: tag ID
@@ -85,12 +73,9 @@ tag_info <- function(x, tag, time, type){
                     LAS: last record
                     ")
         }
-
-    }
-        
-    #detach(x)    
-
     
+    return(PIT)
+
 } # Function ends
 
 ###############################################################################
