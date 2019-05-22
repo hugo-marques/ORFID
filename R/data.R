f00bab7b8e1d62f088ec2802dbc5a98fe3f75cfb
Hugo Marques <biohmarques@gmail.com>
2019-05-22 01:21:39 -0300

    Bug fixed.

--- R/data.R
+++ R/data.R
@@ -5,15 +5,17 @@
 #'
 #' Dataset to illustrate the ORFID functions. The variables are as follows:
 #'
-#' @format A data frame with 460 rows and 9 variables:
+#' @format A data frame with 8590 rows and 9 variables:
 #' \describe{
 #'   \item{DTY}{Detection type (S = summary, I = individual, E = event)}
 #'   \item{ARR}{Arrival date and time}
+#'   \item{TRF}{Time reference (G = GNSS, N = network, U = unreferenced)}
 #'   \item{DUR}{Duration of the period between the arrival and the departure from the detection zone}
 #'   \item{TTY}{Tag type A=Animal (R = Read only, W = Writeable, P = Phantom)}
 #'   \item{TAG}{Tag ID number}
 #'   \item{SCD}{Site code}
 #'   \item{NCD}{Number of consecutive detections}
+#'   \item{EFA}{Effective amps}
 #' }
 "reader_1"
 
@@ -27,11 +29,13 @@
 #' \describe{
 #'   \item{DTY}{Detection type (S = summary, I = individual, E = event)}
 #'   \item{ARR}{Arrival date and time}
+#'   \item{TRF}{Time reference (G = GNSS, N = network, U = unreferenced)}
 #'   \item{DUR}{Duration of the period between the arrival and the departure from the detection zone}
 #'   \item{TTY}{Tag type A=Animal (R = Read only, W = Writeable, P = Phantom)}
 #'   \item{TAG}{Tag ID number}
 #'   \item{SCD}{Site code}
 #'   \item{NCD}{Number of consecutive detections}
+#'   \item{EFA}{Effective amps}
 #' }
 "reader_2"
 
