2ed3bbb61c86a825ed7895331c25b2c4ba5e03ea
hugo-marques <biohmarques@gmail.com>
2020-03-14 15:00:18 -0700

    UPDATE

--- R/join_ORFID_data.R
+++ R/join_ORFID_data.R
@@ -1,7 +1,7 @@
 #' @title Combine data from ORFID readers into an multi readers array
 #' @description Function allows users to combine unique readers into an array, using the data obtained from \code{\link{import_ORFID}} function. It is necessary to create a list with all data frames to be joined. If the data frames have diferent variables,
 #' @param x list: A list containing data frames created with the \code{\link{import_ORFID}} function to be combined.
-#' @details Data frame is created in the user environment. The output of \code{\link{join_multireader_data}} will contain a column if that column appears in any of data frames combined. A factor class variable called LOC (from location) is the combination of SCD (Site Code) and ANT (Antenna) variables. It should be used as location in further statistical analysis once it is the individual detection spot.
+#' @details Data frame is created in the user environment. The output of \code{\link{join_ORFID_data}} will contain a column if that column appears in any of data frames combined. A factor class variable called LOC (from location) is the combination of SCD (Site Code) and ANT (Antenna) variables. It should be used as location in further statistical analysis once it is the individual detection spot.
 #' @return Returns a tibble object.
 #' @author Hugo Marques <biohmarques@@gmail.com>
 #' @seealso \code{\link{import_ORFID}} for importing data files from the new generation of Oregon RFID readers
